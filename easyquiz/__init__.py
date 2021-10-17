7c166db28aa87cff01db2c5f73ddcb31bdcd5138
Pablo Lapeña <pablolape@gmail.com>
2021-10-17 12:05:52 +0200

    changed version to 0.2

--- easyquiz/__init__.py
+++ easyquiz/__init__.py
@@ -1,8 +1,8 @@
 from .easyquiz import Quiz, QuizQuestion
 
-__version__ = '0.1'
+__version__ = '0.2'
 __author__ = 'Pablo Lapeña Mañero'
 __contact__ = 'plapenamanero@gmail.com'
-__url__ = 'to_do'
+__url__ = 'https://github.com/plapenamanero/easyquiz'
 __description__ = 'Quick and easy quiz creation for IPython'
 __keywords__ = 'quiz IPython'
